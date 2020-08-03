679829621fc12ed80278509c32689b88ef2490cc
Abdel Mejia <bhalut.bear@gmail.com>
2020-08-03 23:30:10 +0000

    Add 0-create_a_file.pp at 0x0A-configuration_management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
+# create a file in /tmp
 file { '/tmp/holberton':
-    path    => '/tmp/holberton',
+    ensure  => 'file',
+    mode    => '0744',
     owner   => 'www-data',
     group   => 'www-data',
     content => 'I love Puppet',
-    mode    => '0744',
 }
