4addf8366ae6e84943f19354f8fdea476bd2f7e2
Abdel Mejia <bhalut.bear@gmail.com>
2020-12-09 00:41:45 +0000

    Add 4-puppet_ssh_config.pp at 0x0B-ssh

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,3 +1,12 @@
-exec {'/bin/echo " IdentityFile ~/.ssh/holberton
-  PasswordAuthentication no" >> /etc/ssh/ssh_config'
+# client SSH configuration file so that you can connect to a server without typing a password.
+file_line { 'private key':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/holberton',
+  ensure => 'absent',
+}
+
+file_line { 'efuse to authenticate':
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  ensure => 'absent',
 }
