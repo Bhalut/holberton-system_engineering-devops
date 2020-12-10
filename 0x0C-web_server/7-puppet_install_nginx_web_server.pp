1fb1cd40aa0326a5da8bcfd217bc4fcb17cc8c05
Abdel Mejia <bhalut.bear@gmail.com>
2020-12-10 22:16:40 +0000

    Fixed 7-puppet_install_nginx_web_server.pp at 0x0C-web_server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -6,11 +6,10 @@ package { 'nginx':
 file { '/var/www/html/index.html':
   content => 'Holberton School',
 }
-
 exec {"/usr/bin/env ufw allow 'Nginx HTTP'":
 }
 
-exec {'/usr/bin/env sed -i "/listen 80 default_server;/a return 301 ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default': }
+exec {'/usr/bin/env sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default': }
 
 service { 'nginx':
   ensure  => running,
