da0d6ab2d2d8fe664f04053cb1c54e7a67a052b3
Abdel Mejia <bhalut.bear@gmail.com>
2020-12-10 04:58:10 +0000

    Add 7-puppet_install_nginx_web_server.pp at 0x0C-web_server

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,18 @@
+#puppet install nginx
+package { 'nginx':
+  ensure => installed,
+}
+
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
+}
+
+exec {"/usr/bin/env ufw allow 'Nginx HTTP'":
+}
+
+exec {'/usr/bin/env sed -i "/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default': }
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
