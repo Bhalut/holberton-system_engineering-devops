b51c1bdcc2597568c9f645cefa9ebaf36a72b5ab
Abdel Mejia <bhalut.bear@gmail.com>
2021-02-02 04:15:28 +0000

    Add 0-gather_data_from_an_API.py file at 0x15-api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,7 +2,7 @@
 """ 0-gather_data_from_an_API
 
     Given employee ID, returns information
-    about his/her TODO list progress.
+    about his/her todo list progress.
 """
 import requests
 import sys
@@ -21,7 +21,8 @@ def main():
              for task in request_todo if task.get('completed') is True]
 
     print('Employee {} is done with tasks({}/{}):'.format(name,
-                                                          len(tasks), len(request_todo)))
+                                                          len(tasks),
+                                                          len(request_todo)))
     print('\n'.join('\t {}'.format(task) for task in tasks))
 
 
