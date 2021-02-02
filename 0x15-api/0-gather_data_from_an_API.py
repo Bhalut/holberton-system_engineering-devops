ce652c82db7f69825cc3fe364a41a7e66e6d4cec
Abdel Mejia <bhalut.bear@gmail.com>
2021-02-02 04:08:50 +0000

    Add 0-gather_data_from_an_API.py file at 0x15-api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -13,15 +13,15 @@ def main():
     """
     user_id = sys.argv[1]
     user = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-    todo = 'https://jsonplaceholder.typicode.com/todos/?userId={}'
-    .format(user_id)
+    todos = 'https://jsonplaceholder.typicode.com/todos/?userId={}'.format(
+        user_id)
     name = requests.get(user).json().get('name')
-    request_todo = requests.get(todo).json()
+    request_todo = requests.get(todos).json()
     tasks = [task.get('title')
              for task in request_todo if task.get('completed') is True]
 
-    print('Employee {} is done with tasks({}/{}):'
-          .format(name, len(tasks), len(request_todo)))
+    print('Employee {} is done with tasks({}/{}):'.format(name,
+                                                          len(tasks), len(request_todo)))
     print('\n'.join('\t {}'.format(task) for task in tasks))
 
 
